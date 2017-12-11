7212d78b303d66226c399f3cc969d53ce81d34d6
Adrian Zniszczyński <a.zniszczynski@nowaera.pl>
2017-12-11 18:52:09 +0100

    Change size of text using percentages

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,23 @@
 $(document).ready(function () {
     $(".navMain__changeLetters__increase").click(function () {
-            const fontSize = $('body').data('font-size');
-            $('body').data(
-                'font-size',
-                fontSize + 5
-            );
-            //"Hack because of problem with crossbrowser compatibility in CSS attr"
-            $("body").css(
-                "font-size",
-                $("body").data('font-size') + "%"
-            );
-        }),
-        $(".navMain__changeLetters__reducing").click(function () {
-            const fontSize = $('body').data('font-size');
-            $('body').data(
-                'font-size',
-                fontSize - 5
-            );
-            //"Hack because of problem with crossbrowser compatibility in CSS attr"
-            $("body").css(
-                "font-size",
-                $("body").data('font-size') + "%"
-            );
-        })
+        $('body').attr(
+            'data-font-size',
+            parseInt($('body').attr('data-font-size')) + 5
+        );
+        updateGlobalFontSize();
+    });
+    $(".navMain__changeLetters__reducing").click(function () {
+        $('body').attr(
+            'data-font-size',
+            parseInt($('body').attr('data-font-size')) - 5
+        );
+        updateGlobalFontSize();
+    });
+    const updateGlobalFontSize = () => {
+        //"Hack because of problem with crossbrowser compatibility in CSS attr"
+        $("body").css(
+            "font-size",
+            parseInt($("body").attr('data-font-size')) + "%"
+        );
+    };
 });
